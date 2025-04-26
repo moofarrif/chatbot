27098144b7fad1fb25907ad0e4d2ddd1c128af5f
Jhon Moofarrif <jhon.moofarrif@gmail.com>
2025-04-25 20:36:17 -0500

    Add GET endpoint for WhatsApp message verification

--- src/whatsapp/whatsapp.controller.ts
+++ src/whatsapp/whatsapp.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { WhatsAppService } from './whatsapp.service';
+import { Request, Response } from 'express';
 
 @Controller('whatsapp')
 export class WhatsAppController {
@@ -10,5 +11,22 @@ export class WhatsAppController {
     const phone = '1234567890'; // El número de teléfono del destinatario
     const message = 'Hola, ¿cómo puedo ayudarte?'; // El mensaje a enviar
     await this.whatsappService.handleIncomingMessage(phone, message);
+  }
+
+  @Get('send-message')
+  async sendMessageGet(@Req() req: Request, @Res() res: Response) {
+    const mode = req.query['hub.mode'] as string;
+    const token = req.query['hub.verify_token'] as string;
+    const challenge = req.query['hub.challenge'] as string;
+
+    const VERIFY_TOKEN = process.env.WHATSAPP_VERIFY_TOKEN;
+
+    console.log('mode:', mode);
+    console.log('token:', token);
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      res.status(200).send(challenge);
+    } else {
+      res.status(403).send('Token inválido');
+    }
   }
 }
